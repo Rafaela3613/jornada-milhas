098a983c2e034b810191ecbbf26f74662a0f3520
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-10 15:53:25 -0300

    criando o card depoimentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './shared/card/card.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { BannerComponent } from './shared/banner/banner.component';
+import { CardComponent } from './shared/card/card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { ContainerComponent } from './shared/container/container.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
-import {MatCardModule} from '@angular/material/card';
-
+import { MatCardModule } from '@angular/material/card';
+import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    FooterComponent,
     HeaderComponent,
     BannerComponent,
+    CardComponent,
     ContainerComponent,
     HomeComponent,
-    CardBuscaComponent
+    FooterComponent,
+    CardBuscaComponent,
+    CardDepoimentoComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +36,6 @@ import {MatCardModule} from '@angular/material/card';
     MatButtonModule,
     MatCardModule
   ],
-
   providers: [],
   bootstrap: [AppComponent]
 })
