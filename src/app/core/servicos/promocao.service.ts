86f66a6931149ca509178f2d4a21f7d2bfca024c
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-12 08:53:11 -0300

    Manipulando variáveis de ambiente

---
+++ src/app/core/servicos/promocao.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Promocao } from '../types/type';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PromocaoService {
+
+  private apiUrl: string = environment.apiUrl;
+
+  constructor(
+          private httpClient: HttpClient
+      ) { }
+
+      listar() : Observable<Promocao[]>{
+          return this.httpClient.get<Promocao[]>(`${this.apiUrl}/promocoes`)
+      }
+}
+
+
