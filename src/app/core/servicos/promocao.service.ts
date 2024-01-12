9c2e2c35d05a7b8ca245eb6c603b99e1c65e3d6c
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-12 15:23:48 -0300

    incluindo o promocoes component

--- src/app/core/servicos/promocao.service.ts
+++ src/app/core/servicos/promocao.service.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Promocao } from '../types/type';
 import { Observable } from 'rxjs';
+import { Promocao } from '../types/type';
 import { environment } from 'src/environments/environment';
-
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +12,10 @@ export class PromocaoService {
   private apiUrl: string = environment.apiUrl;
 
   constructor(
-          private httpClient: HttpClient
-      ) { }
+    private httpClient: HttpClient
+  ) { }
 
-      listar() : Observable<Promocao[]>{
-          return this.httpClient.get<Promocao[]>(`${this.apiUrl}/promocoes`)
-      }
+  listar () : Observable<Promocao[]> {
+    return this.httpClient.get<Promocao[]>(`${this.apiUrl}/promocoes`)
+  }
 }
-
-
