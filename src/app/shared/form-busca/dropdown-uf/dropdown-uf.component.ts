2a79435f94a5d95fbddfa0ee415b4fa1af4a8a79
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-13 10:20:23 -0300

    Recebendo os inputs

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown-uf',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./dropdown-uf.component.scss']
 })
 export class DropdownUfComponent {
-label: any;
-matPrefix: any;
-
+  @Input() label: string = '';
+  @Input() iconePrefixo: string = '';
+  filteredOptions =[];
 }
