834b2043dd9f7ef819914357ee01b803d36cdf3f
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-24 10:15:56 -0300

    Listando as passagens

--- src/app/pages/busca/busca.component.ts
+++ src/app/pages/busca/busca.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PassagensService } from '../../core/services/passagens.service';
 
 @Component({
   selector: 'app-busca',
   templateUrl: './busca.component.html',
   styleUrls: ['./busca.component.scss']
 })
-export class BuscaComponent {
+export class BuscaComponent implements OnInit {
+  [x: string]: any;
+  constructor(private passagensService: PassagensService) {}
+  ngOnInit(): void {
+    const buscaPadrao = {
+      data: new Date().toISOString,
+      pagina: 1,
+      porPagina: 25,
+      somenteIda: false,
+      passageirosAdultos: 1,
+      tipo: "Executiva"
+    }
+
+    this.passagensService.getPassagens(buscaPadrao).subscribe((res: any) => console.log(res)
+    )
+  }
 
 }
