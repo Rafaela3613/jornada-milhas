7e23671df413f253fd7f9c88df0e967f564b4d7b
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-28 19:46:27 -0300

    Resolvendo erros

--- src/app/home/depoimentos/depoimentos.component.ts
+++ src/app/home/depoimentos/depoimentos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DepoimentoService } from 'src/app/home/services/depoimento.service';
 import { Depoimento } from 'src/app/core/types/type';
 
@@ -7,15 +7,15 @@ import { Depoimento } from 'src/app/core/types/type';
   templateUrl: './depoimentos.component.html',
   styleUrls: ['./depoimentos.component.scss']
 })
-export class DepoimentosComponent {
+export class DepoimentosComponent implements OnInit{
   depoimentos: Depoimento[] = [];
   constructor(private service: DepoimentoService) {
   }
   ngOnInit(): void {
     this.service.listar().subscribe(
-      res => {
-        this.depoimentos = res;
-      }
+    res => {
+    this.depoimentos = res;
+    }
     )
   }
 }
